a90a8c5da79e45e4f878ad868eb1a2e915eb4331
benomar aymane <aymane@benomars-MacBook-Air.local>
2025-08-27 21:12:09 +0100

    main commit (schema app)

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,44 @@
+import { useTheme } from "@/hooks/useTheme"
+import { Ionicons } from "@expo/vector-icons"
+import { Tabs } from 'expo-router'
+import React from 'react'
+
+const Tabslayout = () => {
+	const {colors} = useTheme();
+  return (
+	<Tabs screenOptions={{
+		tabBarActiveTintColor: colors.primary,
+		tabBarInactiveTintColor: colors.textMuted,
+		tabBarStyle: {
+			backgroundColor: colors.surface,
+			borderTopWidth: 1,
+			borderTopColor: colors.border,
+			height: 90,
+			paddingBottom: 30,
+			paddingTop: 10,
+		},
+		tabBarLabelStyle: {
+			fontSize: 12,
+			fontWeight: "500",
+		},
+		headerShown: false,
+	}}>
+		<Tabs.Screen name='index'
+		options = {{
+			title: "Tasks",
+			tabBarIcon: ({color, size}) =>
+				<Ionicons name='flash-outline' size={size} color={color}/>,
+	}}
+	/>
+	<Tabs.Screen name='settings'
+		options = {{
+			title: "Settings", 
+			tabBarIcon: ({color, size}) =>
+				<Ionicons name='settings' size={size} color={color}/>,
+	}}
+	/>
+	</Tabs>
+  )
+}
+
+export default Tabslayout
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,31 @@
+import { useTheme } from "@/hooks/useTheme"
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function Index() {
+  const { toggleDarkMode } = useTheme();
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.content}>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.content}>Hi Well this is just the start </Text>
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text>Press Mode</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "orange",
+  },
+
+  content: {
+    color: "white",
+    fontSize: 12,
+  },
+});
